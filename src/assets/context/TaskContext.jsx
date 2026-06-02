99357fa3df53e0cbe0091ada73d2ecfcfa6a992c
Muhammad Abbas <itsmuhammadabbas66@gmail.com>
2026-06-03 01:32:07 +0500

    small change of Api

--- src/assets/context/TaskContext.jsx
+++ src/assets/context/TaskContext.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const TaskContext = createContext()
 
-const API = 'http://localhost:5000/api/tasks'
+const API = '/api';
 
 export function TaskProvider({ children }) {
 
